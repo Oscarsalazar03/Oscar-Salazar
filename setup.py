93558f87398f3401b75d8919c09c7b997b4b1ba0
Oscarsalazar03 <oscar.salazar@est.iudigital.edu.co>
2025-03-01 16:32:47 +0000

    Primer Commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pad",
+    version="0.0.1",
+    author="Óscar Alexander Salazar Alvarez",
+    author_email="Oscar.salazar@est.iudigital.edu.co",
+    description="",
+    py_modules=["actividad_1"],
+    install_requires=[
+        "pandas",
+        "matplotlib"
+    ]
+    
+    
+)
